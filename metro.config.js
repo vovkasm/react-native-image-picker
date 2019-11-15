dbab8bca3437d3db6bb27ceb2dcc28c7f3e5b3f7
Vladimir Timofeev <vovkasm@gmail.com>
2019-11-15 12:32:36 +0300

    refactor example for android

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+'use strict'
+const path = require('path')
+
+module.exports = {
+  projectRoot: path.resolve(__dirname, 'example'),
+  watchFolders: [__dirname],
+
+  resolver: {
+    extraNodeModules: {
+      '@vovkasm/react-native-image-picker': __dirname,
+    }
+  }
+}
